12fb54a431a748cb97a3c685f9bf162db43bcd9c
liujingjing <liujingjing@dipont.com>
2020-07-22 18:00:53 +0800

    update snapshots

--- .jest.js
+++ .jest.js
@@ -6,9 +6,10 @@ const transformIgnorePatterns = [
 ];
 
 module.exports = {
+  // rootDir  默认值：当前目录，一般是package.json所在的目录。
   testURL: 'http://localhost/',
   setupFiles: ['./tests/setup.js'],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'md', 'jpg'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'md', 'jpg'], // 测试文件的类型
   modulePathIgnorePatterns: ['/_site/'],
   testPathIgnorePatterns: ['/node_modules/', 'node'],
   transform: {
@@ -16,7 +17,8 @@ module.exports = {
     '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.svg$': '<rootDir>/node_modules/jest-transform-stub',
   },
-  testRegex: libDir === 'dist' ? 'demo\\.test\\.js$' : '.*\\.test\\.js$',
+  // testRegex: libDir === 'dist' ? 'demo\\.test\\.js$' : '.*\\.test\\.js$',
+  testRegex: 'button\\.test\\.js$', // 设置识别哪些文件是测试文件
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     'ant-design-vue$': '<rootDir>/components/index.js',
